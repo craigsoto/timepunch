c8c82750b859e910d38b8b7ba0c48669a6670e34
Craig Soto <csoto@mines.edu>
2013-04-16 03:40:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timepunch::Application.config.secret_token = '51e59569d484abda339524e20a8d7ae0cf9f4e77b4acb8991192b650d81f6a2725fb7f76948fb21ea0b9a84f3583c7b690d60aea38124e01c8e2d0fa9e417d4e'
